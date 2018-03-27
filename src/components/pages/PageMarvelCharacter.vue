a4840904c05b3427e160b64ea7b652d1bb23c95f
Magno Biét <magno.biet@gmail.com>
2018-03-27 10:49:55 -0300

    Remove unused

--- src/components/pages/PageMarvelCharacter.vue
+++ src/components/pages/PageMarvelCharacter.vue
@@ -67,8 +67,7 @@
 	export default {
 		name: 'PageMarvelCharacter',
 		data: () => ({
-			show: false,
-			lorem: 'Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.'
+			show: false
 		}),
 		computed: {
 			...mapGetters([
